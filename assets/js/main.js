c2a7468ae100ab74c705e65b63eb161252003282
rknight8 <40279117@live.napier.ac.uk>
2019-04-01 21:31:25 +0100

    :godmode: added jquery and started creating functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-alert("Hello There!");
+$(document).ready(function(){
+  $(document).keypress(function(key){
+    if(key.which === 13 && $('#user-input').is(':focus')) {
+      var value = $('#user-input').val();
+      alert(value);
+    }
+  });
+
+});
