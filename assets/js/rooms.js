6f9eba4492c6344b760a7934c8b36c6f7d158444
rknight8 <40279117@live.napier.ac.uk>
2019-04-02 20:39:05 +0100

    :godmode: adding in logic

--- assets/js/rooms.js
+++ assets/js/rooms.js
@@ -7,18 +7,24 @@ var rooms = {
   },
   "crewQuarters": {
     "description": "You wake to the sound of an alarm. The ground beneath you shakes as you steady yourself against a wall. You recognise the alarm as the one to abandon ship. You remember there being an escape pod on the upper deck. You need escape G-34B.",
-    "action": {
+    "direction": {
       "go to door": "button"
       }
   },
   "button": {
     "description": "You approach the door expecting it to open automatically but it does not. To you right, there is a panel with a button that reads: In case of emergency, push to open.",
-    "action": {
-      "push button": "button"
+    "direction": {
+      "push button": "corridor1"
       }
   },
   "corridor1": {
-    "description": "With the alarms still ringing in your ears, you look around and see two doors, one to the west and one to the east at the far end of the corridor. There is a ladder in the centre of the corridor. Coughing, you notice smoke above you. You will not survive long here. You don’t think you can go up the ladder without some sort of gas mask to protect you from the smoke."
+    "description": "With the alarms still ringing in your ears, you look around and see two doors, one to the west and one to the east at the far end of the corridor. There is a ladder in the centre of the corridor. Coughing, you notice smoke above you. You will not survive long here. You don’t think you can go up the ladder without some sort of gas mask to protect you from the smoke.",
+    "direction": {
+      "go up ladder": "ladder"
+      }
+  },
+  "ladder": {
+    "description": "As you look up you notice smoke billowing out from a hole in the ceiling. You decide to go up anyway. You reach the top of the ladder; the smoke is thick here. Covering you mouth with your sleeve the smoke makes your eyes sting. It doesn’t take long for you to lose all sense of direction and the world slowly fades to black. END",
   },
   "kitchen": {
     "description": "In the centre of the room is a small dinning table with enough space to fit a small crew and a kitchen space to your left. A small vent shaft on the wall to your right. The noise of the alarm is a constant sound in your ears. Some emergency equipment has been stashed in one corner of the room."
@@ -33,24 +39,3 @@ var rooms = {
     "description": "Stumbling across the corridor you make it to the door. There is smoke everywhere and the alarm continues to sound. The ship starts to tilt, and you sway on your feet trying to stay upright. Looking ahead, you notice one escape pod left in the room. The other crew members must have made it off the ship. You need to do the same."
   }
 };
-
-$(document).ready(function(){
-  $('.game-text').append("<p>" + rooms.crewQuarters.description + "</p>");
-
-  $(document).keypress(function(key){
-    if(key.which === 13 && $('#user-input').is(':focus')) {
-      var value = $('#user-input').val().toLowerCase();
-      switch(value) {
-        case "look around":
-          alert("you see a room");
-          break;
-        case "go to door":
-          alert("you go over to the door");
-          break;
-        default:
-          alert("Invalid move");
-      }
-    }
-  });
-
-});
