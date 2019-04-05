76a4c72d3bbec112a86d05588f6d3946f53e8f10
rknight8 <40279117@live.napier.ac.uk>
2019-04-05 19:01:09 +0100

    :feelsgood: added inventory system

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,47 @@
 var currentRoom = "start";
+var commands = ["go", "look", "action"];
+var inventory = [];
+
+
+// function to change rooms
 function changeRoom(dir) {
     if (rooms[currentRoom].direction[dir] !== undefined) {
         currentRoom = rooms[currentRoom].direction[dir];
         $('#game-text').append("<p>" + rooms[currentRoom].description + "</p>");
-      } else {
-        $('#game-text').append("<p>You cannot go that way.</p>");
+    } else {
+        $('#game-text').append("<p>You cannot go that way!</p>");
       }
 }
 
+
+// function for inventory
+function showInventory() {
+  if(inventory.length === 0) {
+    $('#game-text').append("<p>You are not carrying anything:</p>");
+    return;
+  }
+  $('#game-text').append("<p>Inventory:</p>");
+  $('#game-text').append("<p><ul>");
+  for(var i=0; i < inventory.length; i ++) {
+    $('#game-text').append("<li>" + inventory[i] + "</li>");
+  }
+  $('#game-text').append('</ul></p>');
+}
+
+
+// function & switch to take in player input
 function playerInput(input) {
   var command = input.split(" ")[0];
   switch(command) {
-  case "get up":
-    changeRoom ("get up");
+    case "go":
+      var dir = input.split(" ")[1];
+      changeRoom (dir);
       break;
-    case "north":
-    changeRoom ("north");
+    case "inventory":
+      showInventory();
       break;
-  default:
-    alert("Invalid move");
+    default:
+    $('#game-text').append("<p>Invalid command!</p>");
 }
 
 }
@@ -29,6 +52,7 @@ $(document).ready(function(){
   $(document).keypress(function(key){
     if(key.which === 13 && $('#user-input').is(':focus')) {
       var value = $('#user-input').val().toLowerCase();
+      $('#user-input').val("");
       playerInput(value);
     }
   });
--- assets/js/rooms.js
+++ assets/js/rooms.js
@@ -1,55 +1,55 @@
 var rooms = {
   "start": {
     "description": "You wake to the sound of an alarm. The ground beneath you shakes as you steady yourself against a wall. You recognise the alarm as the one to abandon ship. You remember there being an escape pod on the upper deck. You need escape G-34B.",
-    "action": {
-      "look around": "crewQuarters"
+    "direction": {
+      "room": "crewQuarters"
     }
   },
 
   "crewQuarters": {
     "description": "You seem to be in the crew’s quarters. Four beds occupy the otherwise empty room. The light from the alarm dances across the room. There is a door on the east wall.",
-    "action": {
-      "look at bed": "easterEgg"
+    "examine": {
+      "look": "easterEgg"
     },
     "direction": {
-      "go to east door": "eastDoor1"
+      "east": "eastDoor1"
       }
   },
 
   "easterEgg": {
     "description": "Upon closer inspection of one of the beds, there is photo of a man with a bowl cut and a large set of teeth. Writing underneath the photograph reads “Dwayne Dibley”.",
-    "action": {
-      "look around": "crewQuarters"
+    "examine": {
+      "room": "crewQuarters"
       }
   },
 
   "eastDoor1": {
     "description": "You approach the door expecting it to open automatically but it does not. To you right, there is a panel with a button that reads: In case of emergency, push to open.",
-    "action": {
-      "look around": "crewQuarters"
+    "examine": {
+      "room": "crewQuarters"
     },
       "direction": {
-      "push button": "button"
+      "push": "button"
       }
   },
 
   "button": {
     "description": "The door lifts open with a whoosh and as you enter the dimly lit corridor you notice smoke above you. You won’t survive long here.",
     "action": {
-      "look around": "corridor1",
+      "room": "corridor1",
     },
     "direction": {
-      "go back": "crewQuarters"
+      "back": "crewQuarters"
     }
   },
 
   "corridor1": {
     "description": "Looking around, there are two doors at the end of a narrow corridor, one to the west and another to the east. In the middle of the corridor is a ladder. You don’t think you can go up the ladder without some sort of equipment to protect you.",
     "direction": {
-      "go up ladder": "ladderEnd",
-      "go back": "crewQuarters",
-      "go to east door": "lockedDoor",
-      "go to west door": "kitchenDoor"
+      "ladder": "ladderEnd",
+      "back": "crewQuarters",
+      "east": "lockedDoor",
+      "west": "kitchenDoor"
       }
   },
 
@@ -59,29 +59,29 @@ var rooms = {
 
   "lockedDoor": {
     "description": "You approach the east door and press the button — nothing happens. You push the button again, but the door doesn’t budge. Looks like the door is stuck. The west door you saw earlier is behind you.",
-    "action": {
-      "look around": "corridor1"
+    "examine": {
+      "room": "corridor1"
     },
     "direction": {
-      "go to ladder": "ladderEnd",
-      "go to west door": "kitchenDoor"
+      "ladder": "ladderEnd",
+      "west": "kitchenDoor"
     }
   },
 
   "kitchenDoor": {
     "description": "You approach the door and notice another emergency panel beside the door. The writing on the panel is corroded. The alarm is still ringing in your ears.",
     "action": {
-      "push button": "kitchenButton"
+      "push": "kitchenButton"
     },
     "direction": {
-      "go to east door": "lockedDoor"
+      "east": "lockedDoor"
     }
   },
 
   "kitchenButton": {
     "description": "Pushing the button, the door starts to lift. It jams halfway from the floor. The ship suddenly shakes violently. You notice the door above you rattling in the doorway, threatening to drop. You need to decide if you want to continue through the door or go back.",
     "direction": {
-      "go back": "doorEnd",
+      "back": "doorEnd",
       "continue": "kitchen"
     }
   },
@@ -93,85 +93,85 @@ var rooms = {
   "kitchen": {
     "description": "You continue onwards and manage to crawl under the door just before the door collapses. As you push yourself off the ground you feel the ship vibrate underneath you. At this rate you’re not sure how the ship will last.",
     "action": {
-      "look around": "kitchen2"
+      "room": "kitchen2"
     }
   },
 
   "kitchen2": {
     "description": "Looking around the small mess hall brings back happy memories. There's a small vent shaft on the wall to your right. Some emergency equipment has been hastily stashed in one corner of the room. The noise of the alarm is a constant sound in your ears.",
     "direction": {
-      "got to vent": "vent1"
+      "vent": "vent1"
     },
-    "action": {
-      "look at equipment": "equipment"
+    "items": {
+      "equipment": ""
     }
   },
 
   "vent1": {
     "description": "You go over to the vent. As you try to open it you notice the vent is secured with four bolts. It looks like you will need some tools to open it.",
-    "action": {
-      "look around": "kitchen2",
-      "look at equipment": "equipment"
+    "examine": {
+      "room": "kitchen2",
+      "equipment": "equipmentAquire"
     }
   },
 
-  "equipment": {
+  "equipmentAquire": {
     "description": "Most of the equipment looks broken but you manage to salvage a gas mask with a cracked eye lens and a wrench.",
     "action": {
-      "look around": "kitchen2"
+      "search": "kitchen2"
     },
     "direction": {
-      "go to vent": "vent2"
+      "vent": "vent2"
     }
   },
 
   "vent2": {
     "description": "With the door jammed you take your chances with the small vent. It is secured on the wall with four bolts.",
     "action": {
-      "use wrench": "corridorSmoke"
+      "wrench": "corridorSmoke"
     },
   },
 
   "corridorSmoke": {
     "description": "Using the wrench, you managed to take the cover off the vent and climb through. You end up in the same corridor you were in previously. The smoke has gotten considerably worse. A gas mask would be useful here.",
     "action": {
-      "look around": "corriderSmokeDes",
-      "use gas mask": "corridorMask"
+      "search": "corriderSmokeDes",
+      "mask": "corridorMask"
     }
   },
 
   "corridorSmokeDes": {
     "description": "You cannot see much in front of you. As far as you can tell the corridor looks the same as it did before only with considerably more smoke. It’s not safe to move without something to protect you from the smoke.",
     "action": {
-      "use gas mask": "corridorMask"
+      "mask": "corridorMask"
     }
   },
 
   "corridorMask": {
     "description": "Putting on the gas mask you can breathe normally. However, the smoke has greatly limited your vision. You remember the ladder being in the middle of the corridor. You don’t want to stay here any longer than necessary.",
     "direction": {
-      "go up ladder": "ladder"
+      "ladder": "ladder2"
     }
   },
 
-  "ladder": {
+  "ladder2": {
     "description": "You climb the ladder with little difficulty and manage to pull yourself up to the upper deck of the ship. You can hardly see a thing. You remember there being escape pods nearby.",
     "action": {
-      "look around": "corridorUpper"
+      "search": "corridorUpper"
     }
   },
 
   "corridorUpper": {
     "description": "You try to focus on your surroundings, but it is hard to see anything through the thick smoke. On the north-east wall you see an open door. Through your mask you can smell something burning. You need to leave this place.",
     "direction": {
-      "go to door": "escapeRoom"
+      "door": "escapeRoom"
     }
   },
 
   "escapeRoom": {
     "description": "Stumbling across the corridor you make it to the door. There is smoke everywhere and the alarm continues to sound. The ship starts to tilt, and you sway on your feet trying to stay upright. Looking ahead, you notice one escape pod left in the room. The other crew members must have made it off the ship. You need to do the same.",
     "direction": {
-      "go to escape pod": "endingGood"
+      "escape": "endingGood"
     }
   },
 
